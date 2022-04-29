99bf50e29fdb3ef02bb35b17db77b8261051fd59
Robert Gittings <rjgitting@gmail.com>
2022-04-28 20:07:34 -0400

    commit tree a0e8146594703b6450eb96fff2ac31f61af8ba51

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin (record) {
+    let outcome = record.find( record => record.result === 'W');
+    if (outcome) {
+  return outcome.year
+} else {
+  return undefined
+}
+}
+
+  /*function win(input){
+    return input.result === `W`
+    //return input.year
+  }
+
+  function superbowlWin (record){
+      let outcome = record.find(win).year
+      if (outcome) {
+      return outcome
+      } else {return undefined}
+  }*/
+
+  /*superbowlWin()
+
+  function sbWin(){
+
+  }
+
+  sbWin record.map.result*/
